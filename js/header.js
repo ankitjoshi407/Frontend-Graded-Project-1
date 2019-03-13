6aa86a61414e8c2c470f1e8a3512ef9513d1a798
ankit <ankit.joshi407@gmail.com>
2019-03-13 14:47:41 +0530

    first commit

---
+++ js/header.js
@@ -0,0 +1,7 @@
+function func2()
+{
+    var modal=document.getElementById('myModal');
+    var modal1=document.getElementById('signupmodal');
+    modal1.style.display="block";
+    modal.style.display="none";
+}
